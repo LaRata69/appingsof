1d1a8f12193a52fe6368a9b5dfb317c4eb532260
alesmms <almma.laoi@gmail.com>
2021-06-10 21:52:41 -0400

    Primer commit

---
+++ app/Models/Profesor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profesor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'rut_profesor',
+        'nombre_profesor',
+        'correo',
+        'es_encargado',
+        'estado',
+    ];
+}
---
+++ database/migrations/2021_06_10_044310_create_asignaturas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsignaturasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asignaturas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nrc');
+            $table->integer('codigo_asignatura');
+            $table->string('rut_profesor');
+            $table->string('nombre_profesor');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asignaturas');
+    }
+}
